4c6bdd28fe4ed9651953fb05e05fc2edfbfbcb4d
Chris Yoon <chy.yoon@gmail.com>
2020-06-09 11:04:08 -0700

    Add branching if logic to the .js to alert user of negative, NaN and illogical operations. Co-authored-by:  Spencer Moody <spencer.moody@outlook.com> Co-authored-by: Lizzie Van Hook <evanhook360@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,50 @@
 $(document).ready(function() {
   $("#form-one").submit(function(event) {
     event.preventDefault();
-    const num1 = parseInt($("#num1").val());
-    const num2 = parseInt($("#num2").val());
-
-    //assuming num1=20 and num2=5
-    //elements =num1/num2 
+    const num1 = parseInt($("input#num1").val());
+    const num2 = parseInt($("input#num2").val());
+    
+    let elements = num1 / num2; 
     let array=[];
-    for(let i=0; i<elements; i++) {
-       array.push(i * num2);
-    }
-    console.log(array);
+
+    if(num1, num2) {
+      if (num2 > num1) {
+        alert("Please enter a larger 'count to' number");
+      }
+      else {
+        if ((num1 <= 0) || (num2 <= 0)) {
+          alert("Please enter a larger number")
+        }
+        else {
+          if (isNaN(num1,num2)) {
+            alert("Please enter a number");  
+          }
+          else {
+            for(let i=0; i<elements; i++) {
+            array.push(i * num2);
+            }
+            stringArray = array.join(", ");
+
+            $(".num1").append(num1);
+            $(".num2").append(num2);
+            $(".array").append(stringArray);
+            $("#output").show();
+          }
+        }
+      }
+  }
+  else {
+    alert ("enter a number you rascal")
+  }
+    
   });
 });
 
-//Businasty logic
+/*if (num1 > num2) {
+  alert("'count to' must be larger");
+} else if (!num1 || !num2) {
+  alert("Please enter a number");
+} else {
+  for(let i=0; i<elements; i++) {
+    array.push(i * num2);
+  }//*/
